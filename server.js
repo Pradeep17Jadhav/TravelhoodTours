0c3710cca7cf9f343d5e84751752aa77f89cfa58
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2021-10-24 17:32:10 +0530

    setup env and packages

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
 const upComingTreks = require("./schema/upComingTreks");
+const PORT = process.env.PORT || 4000;
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
-app.listen(4000, () => console.log("Server Started"));
+app.listen(PORT, () => console.log("Server Started"));
 app.use(cors());
 
 app.get("/hi", (req, res) => {
@@ -42,3 +43,12 @@ app.get("/trek/:id", async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
+
+if(process.env.NODE_ENV == "production")
+{
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res)  => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
