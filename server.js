a95c233a76e037eb33b3ba445c839d1cbd5cc489
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2021-10-24 18:43:26 +0530

    comment out sendfile code

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ app.get("/trek/:id", async (req, res) => {
 if(process.env.NODE_ENV == "production")
 {
     app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*", (req, res)  => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    })
+    // const path = require("path");
+    // app.get("*", (req, res)  => {
+    //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    // })
 }
